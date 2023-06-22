5777cbb8b86abc6b0735efc8d0d5b4252a41002d
Manuel Rivera <mukhuve@gmail.com>
2023-06-22 14:41:59 -0500

    login endpoint completed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,31 @@
+import { compare } from "bcrypt";
 import usersRepository from "../repositories/users.repository.js";
+import jwt from "jsonwebtoken";
 
 async function login(req, res) {
   // obtener credenciales
   const credentials = req.body;
   const email = credentials.email;
+  const password = credentials.password;
 
   // buscar usuario
   const user = await usersRepository.byEmail(email);
-  console.log(user);
+  if (!user) {
+    return res.status(404).send("Usuario no encontrado");
+  }
 
-  // comparar contraseña // bcrypt
+  // comparar contraseña
+  const isValid = await compare(password, user.password);
+  if (!isValid) {
+    return res.status(401).send("Contraseña incorrecta!");
+  }
+
   // crear el token // jsonwebtoken
+  const token = jwt.sign({ sub: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
 
-  res.send("Ok!");
+  res.status(201).json({ token });
 }
 
 export default { login };
